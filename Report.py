16c4a45690ac357182a7c2314cf6be045f6464c5
rw-AzwanaAhmad <azwana.ahmad@rockwool.com>
2025-05-15 18:50:00 +0800

    checkbox validate

--- Report.py
+++ Report.py
@@ -25,11 +25,9 @@ sign_in_button.click()
 #Go to REPORTS screen
 report_page  = WebDriverWait (driver,10).until(EC.visibility_of_element_located((By.XPATH,"//app-top-nav/nav/div/nav/div/div/div/a[3]"))).click()
 
-#Check if the value is available in the Factory drop-down list.
+#Access Factory drop-down list
 
-factory_value = WebDriverWait (driver, 10). until(EC.visibility_of_element_located((By.XPATH,"/html/body/app-root/div/div/app-reports-list/div/div/div/form/div[1]/div/mat-form-field/div/div[1]/div/mat-select/div/div[1]"))).click()
-
-#To print out all value in Factory drop-down
+factory_value = WebDriverWait (driver, 10). until(EC.visibility_of_element_located((By.XPATH,"/html/body/app-root/div/div/app-reports-list/div/div/div/form/div[1]/div/mat-form-field/div/div[1]/div/mat-select"))).click()
 
 #Get text of the Factory Drop-down: All factories
 factory_value_AllFactory = WebDriverWait (driver, 10). until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[3]/div[2]/div/div/div/mat-option[1]/span")))
@@ -48,20 +46,52 @@ factory_value_text = factory_value_VAM.text
 
 print({factory_value_text})
 
+#To click the checkbox 
+VAM_checked = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH,"/html/body/div[3]/div[2]/div/div/div/mat-option[2]/mat-pseudo-checkbox"))).click()
+
+#To locate the checkbox element again to check it's state
+VAM_checkbox = driver.find_element(By.XPATH,"/html/body/div[3]/div[2]/div/div/div/mat-option[2]/mat-pseudo-checkbox")
+VAM_checkbox_class = VAM_checkbox.get_attribute("class")
+
+#Validate of the checbox has been selected
+if "mat-pseudo-checkbox-checked" in VAM_checkbox_class:
+    print("VAM Checkbox is selected")
+else:
+    print("VAM Checkbox is not selected")
+
 #Validate the VAM is available in the drop-down
 assert factory_value_text == "VAM", f"Expected 'All factories', but value '{factory_value_text}'"
 print("Validation successful: VAM is available in the drop-down Factory")
 
 
 #Get text of the Factory Drop-down: ROE
-factory_value_ROE = WebDriverWait (driver, 10). until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[3]/div[2]/div/div/div/mat-option[2]/span")))
-factory_value_text = factory_value_VAM.text
+factory_value_ROE = WebDriverWait (driver, 10). until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[3]/div[2]/div/div/div/mat-option[3]/span")))
+factory_value_text = factory_value_ROE.text
 
 print({factory_value_text})
 
+#To click the checkbox 
+ROE_checked = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH,"/html/body/div[3]/div[2]/div/div/div/mat-option[3]/mat-pseudo-checkbox"))).click()
+
+#To locate the checkbox element again to check it's state
+ROE_checkbox = driver.find_element(By.XPATH,"/html/body/div[3]/div[2]/div/div/div/mat-option[3]/mat-pseudo-checkbox")
+ROE_checkbox_class = ROE_checkbox.get_attribute("class")
+
+#Validate of the checbox has been selected
+if "mat-pseudo-checkbox-checked" in ROE_checkbox_class:
+    print("ROE Checkbox is selected")
+else:
+    print("ROE Checkbox is not selected")
+
+
 #Validate the ROE is available in the drop-down
 assert factory_value_text == "ROE", f"Expected 'All factories', but value '{factory_value_text}'"
-
 print("Validation successful: ROE is available in the drop-down Factory")
+
+# Get text of the Factory drop_down : MOS
+factory_value_MOS = WebDriverWait (driver,10).until(EC.visibility_of_element_located((By.XPATH,"/html/body/div[3]/div[2]/div/div/div/mat-option[4]/span")))
+factory_value_text = factory_value_MOS.text
+
+
 
 time.sleep (20)
